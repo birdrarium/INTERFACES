5fea2d002a21b3a552dd2c1a46d9e8cea816ffbe
birdrarium <83173704+birdrarium@users.noreply.github.com>
2022-05-17 00:51:47 +0200

    Update Exercise1.cs

--- Exercise1.cs
+++ Exercise1.cs
@@ -1,14 +1,15 @@
 using System;
+using System.Collections.Generic;
 
 namespace interfaces
 {
-    public interface IFigure
+    public interface IFigure : IComparable<IFigure>
     {
         ConsoleColor figureColor { get; set; }
         double Area();
     }
 
-    public class Circle : IFigure
+    public class Circle : IFigure, IComparable<IFigure>
     {
         public double radius;
         public ConsoleColor FigureColor;
@@ -32,10 +33,19 @@ namespace interfaces
         {
             return $"Figure Color: {figureColor} Area: {Area():F2} Radius: {radius}";
         }
+        public int CompareTo(IFigure obj)
+        {
+            if (this.Area() == 0 || this.Area() < obj.Area())
+                return -1;
+            else if (obj.Area() == 0 || this.Area() > obj.Area())
+                return 1;
+            else
+                return 0;
+        }
 
     }
 
-    public class Rectangle : IFigure
+    public class Rectangle : IFigure, IComparable<IFigure>
     {
         public double width, height;
         public ConsoleColor FigureColor;
@@ -60,8 +70,17 @@ namespace interfaces
         {
             return $"Figure Color: {figureColor} Area: {Area():F2} Width: {width} Height: {height}";
         }
+        public int CompareTo(IFigure obj)
+        {
+            if (this.Area() == 0 || this.Area() < obj.Area())
+                return -1;
+            else if (obj.Area() == 0 || this.Area() > obj.Area())
+                return 1;
+            else
+                return 0;
+        }
     }
-    
+
     class Program
     {
         static void Main(string[] args)
@@ -78,6 +97,8 @@ namespace interfaces
             {
                 Console.WriteLine(obj.ToString());
             }
+
+            Console.WriteLine(figures[0].CompareTo(figures[1])); 
         }
     }
 }
